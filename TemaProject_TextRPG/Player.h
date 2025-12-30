0cb80d45adfb92a02a4ea7b0f0f4233a626ee383
junhee030707-pixel <junhee030707@gmail.com>
2025-12-30 20:35:36 +0900

    Player.h
    헤더파일 추가(선언부)

---
+++ TemaProject_TextRPG/Player.h
@@ -0,0 +1,56 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "Inventory.h"
+
+using namespace std;
+
+class Player
+{
+private:
+    string Name;
+    int LV;
+    int HP;
+    int MaxHP;
+    int Attack;
+    int Experience;
+    int Gold;
+    Inventory Inven;
+
+public:
+    // 생성자
+    Player();
+    Player(const string& name, int lv = 1, int hp = 200, int attack = 30, int exp = 0, int gold = 0);
+
+    // 경험치 관련
+    void GainExperience(int amount);
+    void AddExp(int amount);
+
+    // 데미지 처리
+    void TakeDamage(int amount);
+
+    // 골드 관련
+    void AddGold(int amount);
+    bool SpendGold(int amount);
+
+    // 게터
+    string GetName() const;
+    int GetLV() const;
+    int GetHP() const;
+    int GetMaxHP() const;
+    int GetAttack() const;
+    int GetGold() const;
+    Inventory* GetInventory();
+
+    // 세터
+    void SetName(const string& name);
+    void SetHP(int hp);
+    void SetAttack(int attack);
+    void SetExperience(int exp);
+    void SetLV(int lv);
+    void SetGold(int gold);
+    void SetMaxHP(int maxHp);
+
+    // 상태 출력
+    void ShowStatus() const;
+};
