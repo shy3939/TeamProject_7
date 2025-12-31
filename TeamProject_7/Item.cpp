38e9787438c6bc6f1bc2a27b46f22691bde7a843
tkd6966 <tkd6966@gmail.com>
2025-12-31 14:46:30 +0900

    Item

--- TeamProject_7/Item.cpp
+++ TeamProject_7/Item.cpp
@@ -1,48 +1,35 @@
 ﻿#include <iostream>
 #include <string>
 #include "Item.h"
+#include "player.h"
 
 
-Item::Item(std::string ItemName, int hp, int maxhp, int count) : ItemName_(ItemName), hp_(hp), maxhp_(maxhp), count_(count) {}
-Item::Item(std::string ItemName, int atk, int count) : ItemName_(ItemName), atk_(atk), count_(count) {}
+Item::Item(std::string ItemName, int count) : ItemName_(ItemName), count_(count) {}
 Item::~Item() {}
 
-int Item::GetHp() const { return hp_; }
 
-int Item::GetMaxHp() const { return maxhp_; }
+HpPotion::HpPotion(std::string ItemName, int Count) : Item("HP 포션", Count) {}
 
-int Item::GetAtk() const { return atk_; }
-
-int Item::GetCount() const { return count_; }
-
-void Item::SetHp(int Hp) { hp_ = Hp; }
-
-void Item::SetMaxHp(int MaxHp) { maxhp_ = MaxHp; }
-
-void Item::SetAtk(int Atk) { atk_ = Atk; }
-
-void Item::SetCount(int Count) { count_ = Count; }
-
-
-
-
-HpPotion::HpPotion(int Hp, int MaxHp, int Count) : Item("HP 포션", Hp, MaxHp, Count) {}
-
-void HpPotion::Use()
+void HpPotion::Use(Player& player)
 {
-	if (hp_ < maxhp_)
+	int CurrentHp = player.GetHP();
+	int MaxHp = player.GetMaxHP();
+
+	if (CurrentHp < MaxHp)
 	{
 		if (count_ > 0)
 		{
 			std::cout << ItemName_ << " 사용" << HealAmount_ << "의 체력을 회복했습니다." << std::endl;
-			hp_ += HealAmount_;
+			CurrentHp += HealAmount_;
 
-			if (hp_ > maxhp_)
+			if (CurrentHp > MaxHp)
 			{
-				hp_ = maxhp_;
+				CurrentHp = MaxHp;
 			}
 
-			std::cout << "현재 체력 : " << hp_ << std::endl;
+			player.SetHP(CurrentHp);
+
+			std::cout << "현재 체력 : " << player.GetHP() << std::endl;
 			std::cout << "남은 " << ItemName_ << " 갯수 : " << --count_ << std::endl;
 		}
 		else
@@ -56,15 +43,18 @@ void HpPotion::Use()
 	}
 }
 
-AtkPotion::AtkPotion(int Atk, int Count) : Item("공격력 포션", Atk, Count) {}
+AtkPotion::AtkPotion(std::string ItemName, int Count) : Item("공격력 포션", Count) {}
 
-void AtkPotion::Use()
+void AtkPotion::Use(Player& player)
 {
 	if (count_ > 0)
 	{
+		int nextAtk = player.GetAttack() + AtkAmount_;
+
+		player.SetAttack(nextAtk);
+
 		std::cout << ItemName_ << " 사용" << AtkAmount_ << "의 공격력이 증가했습니다." << std::endl;
-		atk_ += AtkAmount_;
-		std::cout << "현재 공격력 : " << atk_ << std::endl;
+		std::cout << "현재 공격력 : " << player.GetAttack() << std::endl;
 		std::cout << "남은 " << ItemName_ << " 갯수 : " << --count_ << std::endl;
 	}
 	else
