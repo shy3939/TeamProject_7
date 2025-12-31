74299967d5d68e8a51b41e20140b7d0bb826b62c
sihwanyu <kpky33@naver.com>
2025-12-31 11:26:23 +0900

    Monster / Goblin 구현

--- TeamProject_7/Goblin.h
+++ TeamProject_7/Goblin.h
@@ -1,5 +1,22 @@
 ﻿#pragma once
-class Goblin
-{
-};
+#include "Monster.h"
 
+class Goblin : public Monster
+{
+public:
+	Goblin(const Player& player);
+
+	~Goblin() override;
+
+	int MosterAttack() override;
+
+	void InitRandom() override;
+	void RHoldingItem() override;
+
+
+private:
+	int rmaxhp_;
+	int ratk_;
+	int rgold_;
+	int ritem_;
+};
--- TeamProject_7/Monster.cpp
+++ TeamProject_7/Monster.cpp
@@ -1,1 +1,58 @@
 ﻿#include "Monster.h"
+
+Monster::Monster()
+	:monstername_("Unknow"),
+	maxhp_(0),
+	hp_(0),
+	atk_(0),
+	gold_(0)
+{
+}
+
+Monster::~Monster()
+{
+}
+
+std::string Monster::GetMonsterName() const
+{
+	return monstername_;
+}
+int Monster::GetMaxHp() const
+{
+	return maxhp_;
+}
+int Monster::GetHp() const
+{
+	return hp_;
+}
+int Monster::GetATK() const
+{
+	return atk_;
+}
+int Monster::GetGold() const
+{
+	return gold_;
+}
+// 획득한 아이템 출력
+void Monster::SetMaxHp(int maxhp)
+{
+	maxhp_ = maxhp;
+}
+void Monster::SetHp(int hp)
+{
+	hp_ = hp;
+}
+void Monster::SetATK(int atk)
+{
+	atk_ = atk;
+}
+void Monster::SetGold(int gold)
+{
+	gold_ = gold;
+}
+// 획득할 아이템 선택
+
+int Monster::RandomInRange(int min, int max)
+{
+	return rand() % (max - min + 1) + min;
+}
