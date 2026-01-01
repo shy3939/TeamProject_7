8edc9373609de9524bb169f0f8c4707320ccaa12
sihwanyu <kpky33@naver.com>
2026-01-01 22:46:24 +0900

    utf-8.cpp

---
+++ TeamProject_7/utf.cpp
@@ -0,0 +1,35 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <string>
+
+std::string GetUTFInput() {
+    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+    wchar_t wbuffer[1024];
+    DWORD charactersRead = 0;
+    if (!ReadConsoleW(hInput, wbuffer, 1024, &charactersRead, NULL)) {
+        return "";
+    }
+
+    std::wstring wstr(wbuffer, charactersRead);
+    while (!wstr.empty() && (wstr.back() == L'\r' || wstr.back() == L'\n')) {
+        wstr.pop_back();
+    }
+
+    if (wstr.empty()) return "";
+    int size_needed = WideCharToMultiByte(
+        CP_UTF8, 0,
+        wstr.c_str(),
+        static_cast<int>(wstr.size()),
+        NULL, 0, NULL, NULL);
+
+    std::string utf8Str(size_needed, 0);
+
+    WideCharToMultiByte(
+        CP_UTF8, 0,
+        wstr.c_str(),
+        static_cast<int>(wstr.size()),
+        &utf8Str[0],
+        size_needed, NULL, NULL);
+
+    return utf8Str;
+}
