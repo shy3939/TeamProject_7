84e0a1d1f484a53343de7e0338f8f07b943fa4bd
dksktlq5-pixel <dksktlq5@gmail.com>
2026-01-02 10:30:39 +0900

    IsEmpty, IsAvailable, GetItemCount complete
    cpp파일에 IsEmpty, IsAvailable, GetItemCount 선언문 구현

--- TeamProject_7/Inventory.cpp
+++ TeamProject_7/Inventory.cpp
@@ -19,17 +19,25 @@ Inventory::~Inventory()
 
 bool Inventory::IsEmpty() const                               // 맵이 아예 통째로 비여있는지 모든 아이템이 없으면 true 아이템이 하나라도 있으면 false
 {
-	return false;
+	return items_.empty();
 }
 
 bool Inventory::IsAvailable(const std::string& key) const     // key가 들어왔을때 맵에서 key를 찾아서 재고가 0개인지 사용가능한지를 반환.
 {
-	return false;
+	auto it = items_.find(key);
+	if (it == items_.end())
+		return false;
+
+	return it->second > 0;
 }
 
 int Inventory::GetItemCount(const std::string& key) const     //key를 받아와서 key가 해당하는 재고의 갯수를 반환
 {
-	return 0;
+	auto it = items_.find(key);
+	if (it == items_.end())
+		return 0;
+
+	return it->second;
 }
 
 void Inventory::Use(const std::string& key, Player* player)
