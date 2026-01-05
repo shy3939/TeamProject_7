97cfdd07bc3bd8b9876e098d1adb171d62775080
junhee030707-pixel <junhee030707@gmail.com>
2026-01-05 17:05:37 +0900

    Monster 생성자 및 값 추가 및 수정, 고블린 스탯 추가, 플레이어 스탯 추가
    전체 전투 계산 로직 필요 아직 전투로직이 필드에 구체화 되지 않았음.

---
+++ TeamProject_7/BossMonster.cpp
@@ -0,0 +1,36 @@
+﻿#include "BossMonster.h"
+
+//이름: "Boss " + 기존 몬스터 이름
+//hp = base.hp * 1.5
+//atk = base.atk * 1.5
+//gold도 1.5배
+BossMonster::BossMonster(Monster& base_)
+{
+	name_ = "Boss " + base_.GetName();
+	maxhp_ = base_.GetHP() * RandomInRange(1, 1.5);
+	hp_ = maxhp_;
+	atk_ = base_.GetATK() * RandomInRange(1, 1.5);
+	gold_ = base_.GetGold() * RandomInRange(1, 1.5);
+	str_ = base_.GetSTR() * RandomInRange(1, 1.5);
+	agi_ = base_.GetAGI();
+	vit_ = base_.GetVIT() * 1.5;
+	int_ = base_.GetINT() * 1.5;
+	luk_ = base_.GetLUK();
+
+}
+// 보스 변수 string이 포인터일때는 
+// new string("Boss" + base_.GetName());
+
+//몬스터 헤더파일의 버츄어 함수 사용주기 끝나면 종료
+BossMonster::~BossMonster()
+{}
+
+int BossMonster::MonsterATK()
+{
+	return atk_ + str_ + int_;
+}
+
+void BossMonster::TakeDamage(int damage_)
+{
+	this->hp_ -= damage_;
+}
